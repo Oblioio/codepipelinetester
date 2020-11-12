c32fc3f476a30bd8da211f717dd5890f904035b6
sagrimson <scott.agrimson@gmail.com>
2020-11-12 10:04:22 -0800

    update webpack config

--- webpack_config/webpack.config.prod.js
+++ webpack_config/webpack.config.prod.js
@@ -1,12 +1,20 @@
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
 const path = require('path');
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const appDir = path.resolve(__dirname, '..', 'app');
 const distDir = path.resolve(__dirname, '..', '_site');
 
 const entry = {
-  mainjs: './js/main.js'
+  mainjs: './js/main.js',
+  styles: './scss/main.scss',
+  critical: './scss/critical.scss'
 };
 
 const plugins = [
@@ -21,6 +29,21 @@ const plugins = [
   new webpack.LoaderOptionsPlugin({
     minimize: true,
     debug: false,
+  }),
+
+  // Put all css code in this file
+  // new ExtractTextPlugin('css/main.css'),
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: 'css/[name].css',
+    chunkFilename: 'css/[id].css',
+  }),
+
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: 'assets', to: 'assets' }
+    ]
   })
 ];
 
@@ -84,6 +107,36 @@ module.exports = (env) => {
           ],
         },
         {
+          test: /\.(scss|css)$/,
+          include: [
+            appDir,
+          ],
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../',
+              },
+            },
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [require('autoprefixer')({
+                    grid: 'no-autoplace'
+                  })],
+                }
+              },
+            },
+            {
+              loader: 'sass-loader',
+            }
+          ],
+        },
+        {
           test: /\.html$/,
           use: [
             {
@@ -94,12 +147,37 @@ module.exports = (env) => {
               },
             },
           ],
-        }
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf)(\?[a-z0-9]+)?$/,
+          include: [
+            appDir,
+          ],
+          use: 'file-loader?name=assets/fonts/[path]/[name].[ext]',
+        },
+        {
+          test: /\.(mp3|ogg)$/,
+          include: [
+            appDir,
+          ],
+          use: 'file-loader?name=sounds/[name].[ext]',
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|ico|webp|svg)$/,
+          include: [
+            appDir,
+          ],
+          use: [
+            {
+              loader: 'file-loader?name=assets/images/[path][name].[ext]&context=app/assets/images',
+            },
+          ],
+        },
       ],
     },
 
     resolve: {
-      extensions: ['.js'],
+      extensions: ['.js', '.scss', '.css'],
       modules: [appDir, 'node_modules'],
     },
 
